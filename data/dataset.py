13f95ad428d6b5e2b6a9b3238c6657c5a06f5317
SummerMVP <50560767+SummerMVP@users.noreply.github.com>
2025-03-20 08:43:40 +0800

    Update dataset.py
    delete useless

--- data/dataset.py
+++ data/dataset.py
@@ -10,8 +10,6 @@ from torch.utils.data import Dataset
 from PIL import Image
 import cv2
 
-from ..matlab import S_UNIWARD
-
 
 class CoverStegoDataset(Dataset):
 
@@ -61,72 +59,3 @@ class CoverStegoDataset(Dataset):
                 labels.append(label)
 
         return images, labels
-
-
-class OnTheFly(Dataset):
-
-    def __init__(self, cover_dir, num=16, payload=0.4, transform=None):
-        self._transform = transform
-        self._num = num
-        self._payload = payload
-
-        self.cover_label = 0
-        self.stego_label = 1
-
-        self.images = self.get_items(cover_dir)
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, item):
-        image = self.images[item]
-        image = cv2.imread(image, flags=cv2.IMREAD_GRAYSCALE).astype(np.float64)  # HxW
-
-        h, w = image.shape
-        crop_h = np.random.randint(h * 3 // 4, h + 1)
-        crop_w = np.random.randint(w * 3 // 4, w + 1)
-
-        h0s = np.random.randint(0, h - crop_h + 1, (self._num,))
-        w0s = np.random.randint(0, w - crop_w + 1, (self._num,))
-
-        new_images, new_labels = [], []
-        for h0, w0 in zip(h0s, w0s):
-            cover_img = image[h0: h0 + crop_h, w0: w0 + crop_w]
-            new_images.append(cover_img)
-            new_labels.append(self.cover_label)
-
-            stego_img = S_UNIWARD(cover_img, self._payload)
-            new_images.append(stego_img)
-            new_labels.append(self.stego_label)
-
-        idxs = np.random.permutation(len(new_images))  # N
-
-        new_images = np.stack(new_images, axis=0)  # NxHxW
-        new_images = new_images[idxs]
-        new_images = new_images[:, :, :, None]  # NxHxWxC
-
-        new_labels = np.asarray(new_labels)
-        new_labels = new_labels[idxs]
-
-        sample = {
-            'image': new_images,
-            'label': new_labels,
-        }
-
-        if self._transform:
-            sample = self._transform(sample)
-
-        return sample
-
-    @staticmethod
-    def get_items(cover_dir):
-        file_names = sorted(os.listdir(cover_dir))
-
-        images = []
-        for file_name in file_names:
-            image_file = osp.join(cover_dir, file_name)
-            if not osp.isfile(image_file):
-                raise FileNotFoundError('{} not exists'.format(image_file))
-            images.append(image_file)
-
-        return images
